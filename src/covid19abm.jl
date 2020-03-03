92829fa9d4c4474805dfd4df869c28baac8851ef
affan s <affan.s@outlook.com>
2020-03-03 12:06:14 -0500

    initial structure push

--- src/covid19abm.jl
+++ src/covid19abm.jl
@@ -1,5 +1,22 @@
 module covid19abm
+using Parameters, Distributions, StatsBase, StaticArrays, Random
 
-greet() = print("Hello World!")
+@enum HEALTH UNDEF=0 SUSC=1 INF=2 REC=3
+mutable struct Human
+    idx::Int64
+    health::HEALTH
+    swap::HEALTH       
+    age::Int64      # in years. 
+    tis::Int64      # time in state 
+    exp::Int64       # max statetime
+    Human() = new(0, UNDEF, UNDEF, 0, 0, 0)
+end
+
+@with_kw mutable struct ModelParameters @deftype Float64    
+    st::Int64 = 500 ## sim time in days
+    β = 0.0 
+    ii::Int64 = 20    ## initial infected
+end
+export ModelParameters, Human
 
 end # module
