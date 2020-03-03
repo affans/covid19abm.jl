cc4592a6308c5f29b63c9c8580f6de6889728f91
affan s <affan.s@outlook.com>
2020-03-03 11:00:38 -0500

    Files generated by PkgTemplates

---
+++ src/covid19abm.jl
@@ -0,0 +1,5 @@
+module covid19abm
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using covid19abm
+using Test
+
+@testset "covid19abm.jl" begin
+    # Write your own tests here.
+end
