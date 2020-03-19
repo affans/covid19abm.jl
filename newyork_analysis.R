8157ef24e2743bd3c528446c6c37cda6800f4d29
affan s <affan.s@outlook.com>
2020-03-19 12:06:24 -0400

    added code for NY plots

--- newyork_analysis.R
+++ newyork_analysis.R
@@ -28,7 +28,7 @@ gg = ggplot(data=dfm)
 gg = gg + geom_line(aes(x = time, y = value, color=variable), group=("variable"), size=1.2)
 gg = gg + theme_bw()
 gg = gg + ggtitle("Infection Prevalence (New York City)")
-gg = gg + theme(text = element_text(size=15)) 
+gg = gg + theme(text = element_text(size=15))
 gg = gg + scale_color_manual(values=colors, name="Isolation Level", breaks=bvals, labels=lvals)
 gg 
 
@@ -133,54 +133,64 @@ gg
 
 
 ### for scenarios only
-s1 = fread("/data/covid19abm/simresults/newyork/b00485/tau0_f00_q00/timelevel_all.dat")
-s2 = fread("/data/covid19abm/simresults/newyork/b00485/tau1_f05_q00/timelevel_all.dat")
-s3 = fread("/data/covid19abm/simresults/newyork/b00485/tau1_f05_q09/timelevel_all.dat")
-s4 = fread("/data/covid19abm/simresults/newyork/b00485/tau0_f00_q09/timelevel_all.dat")
+
+scenario_data <- function(x){
+  s1 = fread("/data/covid19abm/simresults/newyork/b00485/tau0_f00_q00/timelevel_all.dat")
+  s2 = fread("/data/covid19abm/simresults/newyork/b00485/tau1_f05_q00/timelevel_all.dat")
+  s3 = fread("/data/covid19abm/simresults/newyork/b00485/tau1_f05_q09/timelevel_all.dat")
+  s4 = fread("/data/covid19abm/simresults/newyork/b00485/tau0_f00_q09/timelevel_all.dat")
+  dt = data.table(s1=s1[, get(x)], s2=s2[, get(x)], s3=s3[, get(x)], s4=s4[, get(x)])
+  dt = dt*800 ## 8 mil NYC pop
+  dt$time = 1:500
+  lpm = melt(dt, id.vars = "time", measure.vars = c("s1", "s2", "s3", "s4"))
+  return(lpm)
+}
 
 colors = c("#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e")
-lat_prev = data.table(t=1:500, s1=s1$lat_prev, s2=s2$lat_prev, s3=s3$lat_prev, s4=s4$lat_prev)
-lpm = melt(lat_prev, id.vars = "t", measure.vars = c("s1", "s2", "s3", "s4"))
-gg1 = ggplot(lpm)
-gg1 = gg1 + geom_line(aes(x = t, y = value, color=variable), size=1.2)
+
+ti = scenario_data("lat_prev")
+gg1 = ggplot(ti)
+gg1 = gg1 + geom_line(aes(x = time, y = value, color=variable), size=1.2)
 gg1 = gg1 + theme_bw()
 gg1 = gg1 + ggtitle("Total infections")
 gg1 = gg1 + theme(text = element_text(size=15)) 
 gg1 = gg1 + scale_color_manual(values=colors, name="Scenario", breaks=c("s1", "s4", "s2", "s3"), labels=c("No control", "Shelter in Place 60+ (SiP60+)", "Self-Isolation Symptomatic (SiS)", "SIP60+ (SiP60+ & SiS)"))
 gg1 = gg1 + ylab("Prevalence") + xlab("Time (in days)")
+gg1 = gg1 + scale_y_continuous(labels = comma)
 gg1 
 
-hos_prev = data.table(t=1:500, s1=s1$hos_prev, s2=s2$hos_prev, s3=s3$hos_prev, s4=s4$hos_prev)
-lpm = melt(hos_prev, id.vars = "t", measure.vars = c("s1", "s2", "s3", "s4"))
-gg2 = ggplot(lpm)
-gg2 = gg2 + geom_line(aes(x = t, y = value, color=variable), size=1.2)
+hos = scenario_data("hos_prev")
+gg2 = ggplot(hos)
+gg2 = gg2 + geom_line(aes(x = time, y = value, color=variable), size=1.2)
 gg2 = gg2 + theme_bw()
 gg2 = gg2 + ggtitle("Hospitalization")
 gg2 = gg2 + theme(text = element_text(size=15)) 
 gg2 = gg2 + scale_color_manual(values=colors, name="Scenario", breaks=c("s1", "s4", "s2", "s3"), labels=c("No control", "Shelter in Place 60+ (SiP60+)", "Self-Isolation Symptomatic (SiS)", "SIP60+ (SiP60+ & SiS)"))
 gg2 = gg2 + ylab("Prevalence") + xlab("Time (in days)")
+gg2 = gg2 + scale_y_continuous(labels = comma)
 gg2 
 
-icu_prev = data.table(t=1:500, s1=s1$icu_prev, s2=s2$icu_prev, s3=s3$icu_prev, s4=s4$icu_prev)
-lpm = melt(icu_prev, id.vars = "t", measure.vars = c("s1", "s2", "s3", "s4"))
-gg3 = ggplot(lpm)
-gg3 = gg3 + geom_line(aes(x = t, y = value, color=variable), size=1.2)
+
+icu = scenario_data("icu_prev")
+gg3 = ggplot(icu)
+gg3 = gg3 + geom_line(aes(x = time, y = value, color=variable), size=1.2)
 gg3 = gg3 + theme_bw()
 gg3 = gg3 + ggtitle("ICU")
 gg3 = gg3 + theme(text = element_text(size=15)) 
 gg3 = gg3 + scale_color_manual(values=colors, name="Scenario", breaks=c("s1", "s4", "s2", "s3"), labels=c("No control", "Shelter in Place 60+ (SiP60+)", "Self-Isolation Symptomatic (SiS)", "SIP60+ (SiP60+ & SiS)"))
 gg3 = gg3 + ylab("Prevalence") + xlab("Time (in days)")
+gg3 = gg3 + scale_y_continuous(labels = comma)
 gg3 
 
-ded_prev = data.table(t=1:500, s1=s1$ded_prev, s2=s2$ded_prev, s3=s3$ded_prev, s4=s4$ded_prev)
-lpm = melt(ded_prev, id.vars = "t", measure.vars = c("s1", "s2", "s3", "s4"))
-gg4 = ggplot(lpm)
-gg4 = gg4 + geom_line(aes(x = t, y = value, color=variable), size=1.2)
+ded = scenario_data("ded_prev")
+gg4 = ggplot(ded)
+gg4 = gg4 + geom_line(aes(x = time, y = value, color=variable), size=1.2)
 gg4 = gg4 + theme_bw()
 gg4 = gg4 + ggtitle("Total Deaths")
 gg4 = gg4 + theme(text = element_text(size=15)) 
 gg4 = gg4 + scale_color_manual(values=colors, name="Scenario", breaks=c("s1", "s4", "s2", "s3"), labels=c("No control", "Shelter in Place 60+ (SiP60+)", "Self-Isolation Symptomatic (SiS)", "SIP60+ (SiP60+ & SiS)"))
 gg4 = gg4 + ylab("Total Deaths") + xlab("Time (in days)")
+gg4 = gg4 + scale_y_continuous(labels = comma)
 gg4 
 
 get_legend<-function(myggplot){
@@ -199,12 +209,93 @@ gg4 <- gg4 + theme(legend.position="none")
 # 4. Arrange ggplot2 graphs with a specific width
 grid.arrange(gg1, gg2, legend, gg3, gg4, nrow = 2, ncol=3)
 
-max(s2$hos_prev)
-sum(s1$ded_inc)/sum(s1$lat_inc)
+## Age Specific Case Fatality Rate
+## Look at no isolation case
+ag1 = fread("/data/covid19abm/simresults/newyork/b00485/tau0_f00_q00/timelevel_ag1.dat")
+ag2 = fread("/data/covid19abm/simresults/newyork/b00485/tau0_f00_q00/timelevel_ag2.dat")
+ag3 = fread("/data/covid19abm/simresults/newyork/b00485/tau0_f00_q00/timelevel_ag3.dat")
+ag4 = fread("/data/covid19abm/simresults/newyork/b00485/tau0_f00_q00/timelevel_ag4.dat")
+ag5 = fread("/data/covid19abm/simresults/newyork/b00485/tau0_f00_q00/timelevel_ag5.dat")
+sum(ag1$ded_inc)/sum(ag1$inf_inc)*100
+sum(ag2$ded_inc)/sum(ag2$inf_inc)*100
+sum(ag3$ded_inc)/sum(ag3$inf_inc)*100
+sum(ag4$ded_inc)/sum(ag4$inf_inc)*100
+sum(ag5$ded_inc)/sum(ag5$inf_inc)*100
 
-sum(s1$ded_inc)/(sum(s1$inf_inc) + sum(s1$iiso_inc))
+## Overall CFR (base case no isolation)
+all = fread("/data/covid19abm/simresults/newyork/b00485/tau0_f00_q00/timelevel_all.dat")
+sum(all$ded_inc)/sum(all$lat_in)
+
+## Plots for splitting age specific deaths
+getagdata <- function(x) {
+  ag1 = fread(paste0("/data/covid19abm/simresults/newyork/b00485/", x, "/timelevel_ag1.dat"))
+  ag2 = fread(paste0("/data/covid19abm/simresults/newyork/b00485/", x, "/timelevel_ag2.dat"))
+  ag3 = fread(paste0("/data/covid19abm/simresults/newyork/b00485/", x, "/timelevel_ag3.dat"))
+  ag4 = fread(paste0("/data/covid19abm/simresults/newyork/b00485/", x, "/timelevel_ag4.dat"))
+  ag5 = fread(paste0("/data/covid19abm/simresults/newyork/b00485/", x, "/timelevel_ag5.dat"))
+  dt = data.table(ag1=ag1$ded_prev, ag2=ag2$ded_prev, ag3=ag3$ded_prev, ag4=ag4$ded_prev, ag5=ag5$ded_prev)
+  dt = dt * 800 ## 8 mil NYC pop
+  dt$time = 1:500
+  dt = dt[1:350, ]
+  dtm = melt(dt, id.vars = "time", measure.vars = c("ag1", "ag2", "ag3", "ag4", "ag5"))
+  return(dtm)
+}
+
+getplot <- function(x, scen){
+  gg4 = ggplot(data=x)
+  gg4 = gg4 + geom_line(aes(x = time, y = value, color=variable), size=1.2)
+  gg4 = gg4 + theme_bw()
+  gg4 = gg4 + ggtitle(scen)
+  gg4 = gg4 + theme(text = element_text(size=15)) 
+  gg4 = gg4 + scale_color_manual(values=colors, name="Age Groups", breaks=c("ag1", "ag2", "ag3", "ag4", "ag5"), labels=c("0-4", "5-19", "19-49", "50-64", "65+"))
+  gg4 = gg4 + ylab("Total Deaths") + xlab("Time (in days)")
+  gg4 
+  return(gg4)
+}
+
+s1ag = getagdata("tau0_f00_q00")
+sg1 = getplot(s1ag, "No control")
+
+s2ag = getagdata("tau1_f05_q00")
+sg2 = getplot(s2ag, "Self-Isolation Symptomatic (SiS)")
+
+s3ag = getagdata("tau1_f05_q09")
+sg3 = getplot(s3ag, "SIP60+ (SiP60+ & SiS)")
+
+s4ag = getagdata("tau0_f00_q09")
+sg4 = getplot(s4ag, "Shelter in Place 60+ (SiP60+)")
+
+# extract the legend and remove from the other plots
+sglegend = get_legend(sg1)
+sg1 <- sg1 + theme(legend.position="none")
+sg2 <- sg2 + theme(legend.position="none")
+sg3 <- sg3 + theme(legend.position="none")
+sg4 <- sg4 + theme(legend.position="none")
+# 4. Arrange ggplot2 graphs with a specific width
+grid.arrange(sg1, sg4, sglegend, sg2, sg3,  nrow = 2, ncol=3)
 
 
-sum(s1$ded_inc)/(sum(s1$hos_inc)
+x = "tau0_f00_q00"
+all = fread(paste0("/data/covid19abm/simresults/newyork/b00485/", x, "/timelevel_all.dat"))
+ag1 = fread(paste0("/data/covid19abm/simresults/newyork/b00485/", x, "/timelevel_ag1.dat"))
+ag2 = fread(paste0("/data/covid19abm/simresults/newyork/b00485/", x, "/timelevel_ag2.dat"))
+ag3 = fread(paste0("/data/covid19abm/simresults/newyork/b00485/", x, "/timelevel_ag3.dat"))
+ag4 = fread(paste0("/data/covid19abm/simresults/newyork/b00485/", x, "/timelevel_ag4.dat"))
+ag5 = fread(paste0("/data/covid19abm/simresults/newyork/b00485/", x, "/timelevel_ag5.dat"))
+
+sum(ag1$ded_inc)/sum(ag1$inf_inc)*100
+sum(ag2$ded_inc)/sum(ag2$inf_inc)*100
+sum(ag3$ded_inc)/sum(ag3$inf_inc)*100
+sum(ag4$ded_inc)/sum(ag4$inf_inc)*100
+sum(ag5$ded_inc)/sum(ag5$inf_inc)*100
+
+
+(sum(ag3$ded_inc) + sum(ag4$ded_inc))/(sum(ag3$inf_inc) + sum(ag4$inf_inc))
+(sum(ag3$ded_inc) + sum(ag4$ded_inc) + sum(ag5$ded_inc))/(sum(ag3$inf_inc) + sum(ag4$inf_inc) + sum(ag5$inf_inc))
+
+sum(ag5$ded_inc)/(sum(ag5$ded_inc) + sum(ag3$ded_inc) + sum(ag4$ded_inc) + sum(ag1$ded_inc) + sum(ag2$ded_inc))
+
+
+sum(all$ded_inc)/sum(all$inf_inc)
 
 
